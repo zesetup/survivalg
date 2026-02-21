d117c40677eb042afe20f55dadc84f25d67eb38d
Maksim Paz <paz@iae.lt>
2026-02-21 21:53:44 +0200

    init

---
+++ src/main/java/com/gmail/zerosetup/survivalg/SurvivalgApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.zerosetup.survivalg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvivalgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurvivalgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/zerosetup/survivalg/SurvivalgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.zerosetup.survivalg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurvivalgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
